fc506f3d07f14a3e2ca8026d21ca290479745cdc
Vladimir Fibe <motiw@icloud.com>
2024-04-21 20:27:00 +0500

    Start

--- Netflix/Browse/BrowseRow.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct BrowseRow: Hashable {
-    var index: Int
-    var title: String?
-    var items: [BrowseItem]
-}
---
+++ Netflix/Home/HomeViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
